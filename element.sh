aa231b7942ca4ce90f394102dca97a2b917d5c83
Jodar Angar <josedavidangaritar@gmail.com>
2024-08-24 23:26:20 +0000

    refactor:table

--- element.sh
+++ element.sh
@@ -1,2 +1,26 @@
-PSQL=$($psql --username=freecodecamp --dbname=periodic_table)
-echo "Please provide an element as an argument."
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+
+if [ -z "$1" ]; 
+  then
+    echo "Please provide an element as an argument."
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]
+ then
+  element_data=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $1;")
+  else 
+  element_data=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.symbol = '$1' OR e.name = '$1';")
+fi
+
+if [ -z "$element_data" ]; then
+  echo "Element not found."
+fi
+
+
+IFS='|' read -r atomic_number name symbol type atomic_mass melting_point_celsius boiling_point_celsius <<< "$element_data"
+echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
+
+#The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
+#The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
