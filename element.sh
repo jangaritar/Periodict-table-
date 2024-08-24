e395694a9a1973c450622ca94262afb06dccb128
Jodar Angar <josedavidangaritar@gmail.com>
2024-08-24 23:28:23 +0000

    feat: adding comments

--- element.sh
+++ element.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-
 if [ -z "$1" ]; 
   then
     echo "Please provide an element as an argument."
@@ -13,12 +12,12 @@ if [[ $1 =~ ^[0-9]+$ ]]
   else 
   element_data=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.symbol = '$1' OR e.name = '$1';")
 fi
-
+# Verificar si se encontró un elemento
 if [ -z "$element_data" ]; then
   echo "Element not found."
 fi
-
-
+# Desglosar los datos obtenidos
 IFS='|' read -r atomic_number name symbol type atomic_mass melting_point_celsius boiling_point_celsius <<< "$element_data"
+# Mostrar la información del elemento
 echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
 
